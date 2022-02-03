28537da11f1d2bced960fe14a2ad4b0ef3598ed3
小斌仔 <lybglory@outlook.com>
2022-02-03 22:32:31 +0800

    元组的定义

---
+++ 03_元组/01_元组的定义.py
@@ -0,0 +1,12 @@
+tuple1 = ("海女", "女王的教师", "legalHai", "北国之恋")
+print(tuple1)
+
+# 如果元组中只有一个成员,那么成员后必须写一个逗号
+tuple2 = ("阿部宽",)
+# 小括号也可以省略
+tuple3 = "阿部宽",
+print(tuple2, tuple3)
+
+# 字符串
+tuple4 = "阿部宽"
+print(tuple4)
